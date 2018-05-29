6cb3139c864923e022a31fbdc2264baa29bf2349
RosalynWu <yulin990422@126.com>
2018-05-29 17:54:31 +0800

    公共样式scss文件

--- agl_component/js/index.js
+++ agl_component/js/index.js
@@ -1,4 +1,17 @@
 /**
  * Created by RosalynWu on 2018/5/28.
  */
-angular.module('aglComponents',[])
+(function(){
+    'use strict';
+
+    angular
+        .module("aglComponents",['ui.router'])
+        .config(stateConfig);
+
+    stateConfig.$inject = ['$stateProvider'];
+
+    function stateConfig($stateProvider) {
+        console.log($stateProvider);
+    }
+
+})();

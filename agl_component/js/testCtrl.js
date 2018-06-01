558f3425e309f3a86deee52f67023ade02696cf8
RosalynWu <yulin990422@126.com>
2018-06-01 18:50:40 +0800

    下拉组件（待完成）

--- agl_component/js/testCtrl.js
+++ agl_component/js/testCtrl.js
@@ -8,9 +8,9 @@
         .module('aglComponents')
         .directive('testDirective',testDirective)
         .controller('TestController',testController);
-    testController.$inject = ['$scope','$parse','$$multiMap'];
-    function testController($scope, $parse, $$multiMap){
-
+    testController.$inject = ['$scope','$parse','$templateRequest'];
+    function testController($scope, $parse, $templateRequest){
+        console.log($templateRequest);
         $scope.add = function(x,y){
             return x+y;
         }
@@ -20,11 +20,16 @@
     function testDirective(){
         return{
             restrict: "E",
-            template:"{{result}}",
+            template:"{{result}} <input ng-model=result>",
             scope:{
                 localFun: "&fn"
             },
-            link: function(scope){
+            link: function(scope,elem, attr){
+                console.log(attr);
+                attr.$set('ngModal','new value');
+                attr.$observe('ngModal',function(value){
+                    console.log('这是变化的' + value)
+                });
                 scope.result = scope.localFun({
                     x:5,
                     y:7
--- agl_component/templates/dropdown/dropdown.js
+++ agl_component/templates/dropdown/dropdown.js
@@ -4,8 +4,76 @@
 (function(){
     'use strict';
 
+    var aglDropdownConfig ={
+        appendToOpenClass: "agl-dropdown-open",
+        openClass: 'open'
+    };
+
     angular.module('agl.dropdown',['agl.position', 'agl.multiMap'])
 
+    .constant("AglDropdownConfig",aglDropdownConfig)
+    .service('AglDropdownService',aglDropdownService)
+    .controller('AglDropdownController',aglDropdownController)
+    .directive('aglDropdown',aglDropdown)
+    .directive('aglDropdownToggle',aglDropdownToggle)
+    .directive('aglDropdownMenu',aglDropdownMenu);
 
+    aglDropdownService.$inject = ['$document','$rootScope','$$multiMap'];
+    function aglDropdownService($document, $rootScope, $$multiMap){
+
+    }
+
+    aglDropdownController.$inject = ['$scope', '$element', '$attrs', '$parse'];
+    function aglDropdownController($scope, $element, $attrs, $parse){
+        var self = this,
+            scope = $scope.$new(),
+            getIsOpen,
+            setIsOpen = angular.noop;
+
+        $element.addClass('dropdown');
+
+        this.init = function(){
+            if($attrs.isOpen){
+                getIsOpen = $parse($attrs.isOpen);
+                console.log(getIsOpen);
+                setIsOpen = getIsOpen.assign;
+            }
+        };
+
+        this.isOpen = function(){
+            return scope.isOpen;
+        }
+    }
+
+    aglDropdown.$inject = [];
+    function aglDropdown(){
+        return{
+            controller: 'AglDropdownController',
+            link: function(scope, element, attrs,dropdownCtrl){
+                dropdownCtrl.init();
+            }
+        }
+    }
+
+    aglDropdownToggle.$inject = [];
+    function aglDropdownToggle(){
+        return{
+            require: '?^aglDropdown',
+            link: function(scope, element, attrs, dropdownCtrl){
+                element.addClass('dropdown-toggle');
+            }
+        }
+    }
+
+    aglDropdownMenu.$inject = [];
+    function aglDropdownMenu(){
+        return{
+            restrict: 'A',
+            require: '?^aglDropdown',
+            link: function(scope, element, attrs, dropdownCtrl){
+                element.addClass('dropdown-menu');
+            }
+        }
+    }
 
 })();
